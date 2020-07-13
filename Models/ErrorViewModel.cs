1b663dcc79ab9feb532f5d5076a428f726ebb5e6
unknown <brenomax02@gmail.com>
2020-07-13 10:24:06 -0300

    projeto iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eplayers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
